fd2c98ed00f0372cd1d9d6acbca1ef5652362178
KHT@air <tan.k.h@juno.com>
2022-09-05 18:21:03 -0700

    ch02 working, .http -> .curl working

--- Chapter03/ShoppingCart/Startup.cs
+++ Chapter03/ShoppingCart/Startup.cs
@@ -4,6 +4,7 @@ namespace ShoppingCart
   using System.Linq;
   using Microsoft.AspNetCore.Builder;
   using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.AspNetCore.HttpOverrides;
   using Polly;
 
   public class Startup
@@ -21,10 +22,16 @@ namespace ShoppingCart
             attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)),
             (ex, _) => Console.WriteLine(ex.ToString())));
       services.AddControllers();
+     /*
+      services.Configure<ForwardedHeadersOptions>(options => {
+          options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+      });
+     */
     }
 
     public void Configure(IApplicationBuilder app)
     {
+      // app.UseForwardedHeaders();
       app.UseHttpsRedirection();
       app.UseRouting();
       app.UseEndpoints(endpoints => endpoints.MapControllers());
